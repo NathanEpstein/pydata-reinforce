5a69218cf0461f2869c7dcbdd7d127256c9a1a84
NathanEpstein <ne2210@columbia.edu>
2016-03-24 16:25:51 -0400

    initial creation of encoding for states and actions

--- learn.py
+++ learn.py
@@ -1,9 +1,14 @@
+from encoding import StateActionEncoder
 from rewards import RewardParser
 from transitions import TransitionParser
 from policy import PolicyParser
 
 class MarkovAgent:
-  def __init__(self, observations, dimensions):
+  def __init__(self, observations):
+    self.state_action_encoder = StateActionEncoder(observations)
+    dimensions = self.state_action_encoder.parse_dimensions()
+
+    # transform observation to int encoding before this
     self.reward_parser = RewardParser(observations, dimensions)
     self.transition_parser = TransitionParser(observations, dimensions)
     self.policy_parser = PolicyParser(dimensions)
@@ -12,4 +17,5 @@ class MarkovAgent:
     R = self.reward_parser.rewards()
     P = self.transition_parser.transition_probabilities()
     self.policy = self.policy_parser.policy(P, R)
+    # transform policy from int encoding to string
 
