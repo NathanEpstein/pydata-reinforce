8d3b13cbbd5a637da76df73088cea8a533fed125
NathanEpstein <ne2210@columbia.edu>
2016-03-24 14:45:05 -0400

    "clean up" search and limit array length hardcoding in training samples

--- search.py
+++ search.py
@@ -1,36 +1,35 @@
 from learn import MarkovAgent
-import random_search
-import binary_search
+import search_data
 import random
 
 # simulate agent search
-def perform_agent_search(agent):
+def simulate_agent_search(agent, array_length):
   # create random values and choose the target at random
-  sorted_array = binary_search.random_sorted_ints(25)
+  sorted_array = search_data.random_sorted_ints(array_length)
   target_int = random.choice(sorted_array)
 
   # initialize state and location
-  location = random.choice(range(25))
-  state = binary_search.parse_state(sorted_array, location, target_int)
+  location = random.choice(range(array_length))
+  state = search_data.parse_state(sorted_array, location, target_int)
 
   # loop through the search
-  while (state != 50):
+  while (state != (array_length * 2)):
     print "Found {0} at index {1}. Looking for {2}".format(
       sorted_array[location],
       location,
       target_int
     )
     location = int(agent.policy[state])
-    state = binary_search.parse_state(sorted_array, location, target_int)
+    state = search_data.parse_state(sorted_array, location, target_int)
   print "Found {0} at index {1}!".format(target_int, location)
-  print (sorted_array)
 
-# note that dimensions can be inferred from observations if desired
-# but it is frequently the case that these are known for the application
-DIMENSIONS = { 'state_count': 51, 'action_count': 25 }
-observations = binary_search.observations(250000) + random_search.observations(100000)
+
+# create an agent trained on data
+ARRAY_LENGTH = 25
+DIMENSIONS = { 'state_count': (ARRAY_LENGTH * 2 + 1), 'action_count': ARRAY_LENGTH }
+observations = search_data.observations(1000, ARRAY_LENGTH)
 mark = MarkovAgent(observations, DIMENSIONS)
 mark.learn()
 
 # view search performance of our agent
-perform_agent_search(mark)
+simulate_agent_search(mark, DIMENSIONS['action_count'])
