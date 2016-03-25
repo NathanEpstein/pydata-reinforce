50540a5692de9fcbd790f6a57fd8a182449df53a
NathanEpstein <ne2210@columbia.edu>
2016-03-25 17:29:31 -0400

    increase array length

--- simulate_search.py
+++ simulate_search.py
@@ -2,17 +2,16 @@ from learn import MarkovAgent
 from search import *
 
 simulator = SearchSimulation()
-mark = MarkovAgent(simulator.observations(10000, 5))
+observations = simulator.observations(10000, 20)
+mark = MarkovAgent(observations)
 mark.learn()
 
 class AISearch(Search):
   def update_location(self):
-    print self.state()
-    print mark.policy[self.state()]
     self.location = mark.policy[self.state()]
 
 def print_ai_search():
-  sorted_array = simulator._random_sorted_array(5)
+  sorted_array = simulator._random_sorted_array(20)
   target_int = random.choice(sorted_array)
   search = AISearch(sorted_array, target_int)
   observation = simulator.observation(len(sorted_array), supplied_search = search)
