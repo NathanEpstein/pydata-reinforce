c75eac11992402ed8dc8ac7e2099c5a8b4bd9a37
NathanEpstein <ne2210@columbia.edu>
2016-03-25 17:23:08 -0400

    fix aiSearch bug

--- simulate_search.py
+++ simulate_search.py
@@ -2,15 +2,17 @@ from learn import MarkovAgent
 from search import *
 
 simulator = SearchSimulation()
-mark = MarkovAgent(simulator.observations(100000, 25))
+mark = MarkovAgent(simulator.observations(10000, 5))
 mark.learn()
 
 class AISearch(Search):
   def update_location(self):
-    self.location = mark.policy[self.state]
+    print self.state()
+    print mark.policy[self.state()]
+    self.location = mark.policy[self.state()]
 
 def print_ai_search():
-  sorted_array = simulator._random_sorted_array(25)
+  sorted_array = simulator._random_sorted_array(5)
   target_int = random.choice(sorted_array)
   search = AISearch(sorted_array, target_int)
   observation = simulator.observation(len(sorted_array), supplied_search = search)
